52f69772af8d054f9d62cdee68c0a4ebbfc3d1ba
Wendrew Oliveira <wendrewdevelop@gmail.com>
2024-06-26 15:23:50 -0300

    changes on layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,8 @@
-// add class navbarDark on navbar scroll
-const header = document.querySelector('.navbar');
-console.log(header)
-window.onscroll = function() {
-    const top = window.scrollY;
-    if(top >=100) {
-        header.classList.add('navbarDark');
-    }
-    else {
-        header.classList.remove('navbarDark');
-    }
-}
-// collapse navbar after click on small devices
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navbarSupportedContent')
-
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
-})
+$(document).ready(function(){ 
+	$(".scroll").click(function(event){
+		event.preventDefault();
+		$("html,body").animate({scrollTop:$(this.hash).offset().top}, 500);
+		$('.navbar-default a').removeClass('selected');
+		$(this).addClass('selected');
+    	});
+});
